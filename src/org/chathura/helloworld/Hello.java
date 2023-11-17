9bd25c9fc088fc0e78e600541c01680804548543
chathura226 <chathuralakshan226@gmail.com>
2023-11-17 17:47:57 +0530

    first variable types

---
+++ src/org/chathura/helloworld/Hello.java
@@ -0,0 +1,23 @@
+package org.chathura.helloworld;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        int x=12;
+        long y=21;
+        System.out.println(x);
+        System.out.println(y);
+
+        double mydouble=22.3;
+        System.out.println(mydouble);
+        float myFloat=5.5f;
+        System.out.println(myFloat);
+
+        char myChar='\u00AE';
+        System.out.println(myChar);
+
+        String myString="Chathura";
+        System.out.println(myString);
+    }
+
+}
