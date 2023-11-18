4ac45e4f62e8550f1c28cf45574dc1f329772c20
chathura226 <chathuralakshan226@gmail.com>
2023-11-18 21:13:22 +0530

    basic oop

---
+++ src/org/chathura/oop/Main.java
@@ -0,0 +1,11 @@
+package org.chathura.oop;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Phone  iPhone=new Phone();
+        iPhone.name="iPhone 11";
+        System.out.println(iPhone.name);
+        iPhone.playMusic("Shake it Off");
+    }
+}
---
+++ src/org/chathura/oop/Phone.java
@@ -0,0 +1,13 @@
+package org.chathura.oop;
+
+public class Phone {
+    protected String name;
+    int screenSize;
+    int memory;
+    int camera;
+
+    public void playMusic (String trackName){
+        System.out.println("Now playing "+trackName);
+    }
+
+}
