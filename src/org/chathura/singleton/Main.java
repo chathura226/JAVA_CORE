c9805d5972a1a58ae9a4a15211d51af2f0ae13af
chathura226 <chathuralakshan226@gmail.com>
2023-11-25 15:00:11 +0530

    toString()

--- src/org/chathura/singleton/Main.java
+++ src/org/chathura/singleton/Main.java
@@ -5,5 +5,9 @@ public class Main {
 
         Database database=Database.getInstance("MySql");
         System.out.println(database.getName());
+//        System.out.println(database.toString()); //can use to string to get info about class
+        //if want we can override the toString method ,
+        System.out.println(database.toString());
+
     }
 }
